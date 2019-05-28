883acb04bcb37e56e84702a2bc7b62575f1861c1
高洋 <>
2019-05-28 16:01:22 +0800

    修改

---
+++ GYDemo/GYDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GYDemo
+//
+//  Created by mifit on 2019/5/28.
+//  Copyright © 2019 sai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
